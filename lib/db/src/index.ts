1a6e4f26a6edc745144dcbf33660c90cfd7be779
hashemalsamawi-prog <hashemalsamawi@gmail.com>
2026-07-13 00:20:28 +0300

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -5,12 +5,14 @@ import pg from "pg";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error("DATABASE_URL must be set.");
 }
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 import * as schema from "./schema/index.js";
 export const db = drizzle(pool, { schema });
 
-export * from "./schema/index.js";.
+export * from "./schema/index.js";
